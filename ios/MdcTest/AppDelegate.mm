4b4754e1acde6e68475516ffb833aff391a3960b
Dmitry Gornak <lonnyfox@bk.ru>
2022-06-30 11:48:07 +0300

    splash and icon and first build

--- ios/MdcTest/AppDelegate.mm
+++ ios/MdcTest/AppDelegate.mm
@@ -3,8 +3,9 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
+#import "RNSplashScreen.h"
 #import <React/RCTAppSetupUtils.h>
+
 
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
@@ -55,6 +56,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import { Storage } from './modules/asyncStorage';
 import { FilterProvider } from './modules/filter/context';
 import { OnboardingProvider } from './modules/onboarding/context';
 import { Navigation } from './navigation';
 
 const App = () => {
-  // useEffect(() => {
-  //   Storage.clear();
-  // }, []);
+  useEffect(() => {
+    SplashScreen.hide();
+    // Storage.clear();
+  }, []);
   return (
     <>
       <OnboardingProvider>
