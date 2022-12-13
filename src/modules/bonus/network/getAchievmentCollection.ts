05897dde52172c57fa2e1594f0f385255551d68a
Dmitry Gornak <lonnyfox@bk.ru>
2022-12-14 00:34:39 +0300

    global fixes

--- src/modules/bonus/network/getAchievmentCollection.ts
+++ src/modules/bonus/network/getAchievmentCollection.ts
@@ -1,8 +1,10 @@
+import { Storage } from '../../asyncStorage';
 import { request } from '../../httpClient';
 
 export const getAchievmentCollection = async () => {
+  const user_id = await Storage.getItem('user_id');
   const res = await request({
-    path: 'achievment-collection/list',
+    path: `achievment-collection/list?user_id=${user_id}`,
   });
   return res?.data;
 };
